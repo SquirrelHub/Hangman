78f70a444db5f46b8d69972e364b5f4cc565c545
Risfy <risbysarwar@gmail.com>
2019-08-13 15:18:19 -0400

    2nd commit
    Add potential class files to project

---
+++ src/HangmanLogic.java
@@ -0,0 +1,13 @@
+public class HangmanLogic {
+    // fields
+
+    // constructors
+
+    // setters/ getters
+
+    // method for input validation
+
+    // method for holding list of incorrect inputs
+
+    // method for holding list of correct inputs
+}
