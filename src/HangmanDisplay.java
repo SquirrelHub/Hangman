2f7b177ede8f2fb3436f9e2755765290c3a9e3a6
Risfy <risbysarwar@gmail.com>
2019-08-16 12:29:30 -0400

    1st draft of program
    FML

--- src/HangmanDisplay.java
+++ src/HangmanDisplay.java
@@ -1,2 +1,40 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class HangmanDisplay {
+
+    private ArrayList<Character> wrongInputs;
+    private HangmanLogic hLogic;
+    private String placeholderWord="";
+    private String[] man = {"  0","\n  /","|","\\","\n_", "/", " \\", "_"};
+    private String figure = "", wrongVals ="";
+    private int index = 0;
+
+    public HangmanDisplay(){
+        this.hLogic = new HangmanLogic();
+    }
+
+    // Get input from user
+    public char getCharInput(){
+        char input = (JOptionPane.showInputDialog("Please guess a letter:")).charAt(0);
+        return input;
+    }
+
+    public void displayHangman(){
+        this.figure += man[index];
+        JOptionPane.showMessageDialog(null, figure);
+        index++;
+    }
+
+    public void displayWrongInputs(String word){
+        JOptionPane.showMessageDialog(null, wrongVals);
+    }
+
+    public void displayCorrectInputs(String word){
+        JOptionPane.showMessageDialog(null, word);
+    }
+
+    public void letterAlreadyGuessed(){
+        JOptionPane.showMessageDialog(null, "Letter is already guessed");
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,7 @@
 public class main {
+    public static void main(String[] args){
+        HangmanDisplay test = new HangmanDisplay();
+        test.getCharInput();
+        //test.displayHangman();
+    }
 }
